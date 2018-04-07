b9e729ff1671fb630a42480112673752ad813e91
David Kraft <kraft153@umn.edu>
2018-04-06 23:01:43 -0500

    less-rough draft; some things actually do work

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,50 +1,50 @@
+var crystalValues = [Math.ceil(Math.random() * 12), Math.ceil(Math.random() * 12), Math.ceil(Math.random() * 12), Math.ceil(Math.random() * 12)];
+console.log("Crystal values: " + crystalValues)
+var numberToGuess = Math.ceil(Math.random() * 102 + 18);
+var runningTotal = 0;
+var wins = 0;
+var losses = 0;
+
 // Prevents scripts from executing until page is loaded
 $(document).ready(function () {
 
-    var crystalValues = [Math.ceil(Math.random() * 12), Math.ceil(Math.random() * 12), Math.ceil(Math.random() * 12), Math.ceil(Math.random() * 12)];
-    console.log("Crystal values: " + crystalValues)
-    var numberToGuess = Math.ceil(Math.random() * 102 + 18);
-    $("#target").text("Target: " + numberToGuess);
-    var runningTotal = 0;
-    $("#score").text("Score: " + runningTotal);
-    var wins = 0;
-    $("#wins").text("Wins: " + wins);
-    var losses = 0;
-    $("#losses").text("Losses: " + losses);
+    $("#target").text(numberToGuess);
+    $("#score").text(runningTotal);
+    $("#wins").text(wins);
+    $("#losses").text(losses);
 
     // I really wanted to combine these into a single command, but couldn't figure out how.
-    if (runningTotal < numberToGuess) {
-        $("#crystalRow").on("");
-        $("#crystalRow").on("click", "#crystal1", function () {
-            var runningTotal += crystalValues[1];
-        });
-        $("#crystalRow").on("click", "#crystal2", function () {
-            var runningTotal += crystalValues[2];
-        });
-        $("#crystalRow").on("click", "#crystal3", function () {
-            var runningTotal += crystalValues[3];
-        });
-        $("#crystalRow").on("click", "#crystal4", function () {
-            var runningTotal += crystalValues[4];
-        });
-        $("#score").text("Score: " + runningTotal)
-        else if (runningTotal === numberToGuess) {
+    $(".crystal").on("click", function () {
+        if (runningTotal < numberToGuess) {
+            $("#crystal1").on("click", function () {
+                var runningTotal = runningTotal + crystalValues[1];
+            });
+            $("#crystal2").on("click", function () {
+                var runningTotal = runningTotal + crystalValues[2];
+            });
+            $("#crystal3").on("click", function () {
+                var runningTotal = runningTotal + crystalValues[3];
+            });
+            $("#crystal4").on("click", function () {
+                var runningTotal = runningTotal + crystalValues[4];
+            });
+            $("#score").text(runningTotal);
+        } else if (runningTotal === numberToGuess) {
             var wins = wins++;
             $("wins").text("Wins: " + wins)
             var crystalValues = [Math.ceil(Math.random() * 12), Math.ceil(Math.random() * 12), Math.ceil(Math.random() * 12), Math.ceil(Math.random() * 12)];
             console.log("Crystal values: " + crystalValues);
             var numberToGuess = Math.ceil(Math.random() * 102 + 18);
-            $("#target").text("Target: " + numberToGuess);
+            $("#target").text(numberToGuess);
             var runningTotal = 0;
-        };
-        else if (runningTotal > numberToGuess) {
+        } else if (runningTotal > numberToGuess) {
             var losses = losses++;
             $("losses").text("Losses: " + losses)
             var crystalValues = [Math.ceil(Math.random() * 12), Math.ceil(Math.random() * 12), Math.ceil(Math.random() * 12), Math.ceil(Math.random() * 12)];
             console.log("Crystal values: " + crystalValues);
             var numberToGuess = Math.ceil(Math.random() * 102 + 18);
-            $("#target").text("Target: " + numberToGuess);
+            $("#target").text(numberToGuess);
             var runningTotal = 0;
         };
-    };
-};
+    });
+});
